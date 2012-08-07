8a49c3ea8cb8f4749651ee72107c81fd511de95d
Paolo Bosetti <paolo.bosetti@ing.unitn.it>
2012-08-07 13:53:17 +0200

    Initial Commit

---
+++ peaksearch/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  peaksearch
+//
+//  Created by Paolo Bosetti on 8/7/12.
+//  Copyright (c) 2012 Paolo Bosetti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
+
